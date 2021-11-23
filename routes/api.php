88fe29a7d2c746deb9b5d8253d137a3fa70ae176
Karen Palacios <karenmpalacios@gmail.com>
2021-11-23 12:30:56 -0800

    Change route provider from web to api

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+Route::get('/', function () {
+    return view('auth.login');
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['xss.sanitizer']], function () {
+    Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
+    Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
 });
